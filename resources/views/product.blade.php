eda4360e82f5bb8ab5e47eaa5beeaa754b65bbea
berl10z <dev.berlioz@gmail.com>
2023-06-10 01:36:12 +0600

    about

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,21 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="product py-5">
+    <section class="product h-[60vh] py-5">
         <div class="container px-5 mx-auto">
             <h2 class="title py-5 text-center">Продукт</h2>
             <div class="product-items xl:grid xl:grid-cols-2 md:flex md:flex-col">
                 <div class="column-left space-y-3">
                     <h2 class="about-text">Наименование: {{ $product->name }}</h2>
                     <p class="about-text">Описание: {{ $product->description }}</p>
-                    <p class="about-text">Склад: {{ $product->warehouse->name }}</p>
+                    <p class="about-text">Склад: {{ $product->warehouse->name ?? "-"  }}</p>
                     @if ($product->document)
-                    <p class="about-text">Документ: {{ last(explode('/', $product->document->path)) }}<a download href="{{ asset('storage/'.$product->document->path) }}" style="color: blue"> Скачать</a></p>
+                    <p class="about-text">Документ: {{ last(explode('/', $product->document->path ?? "-")) }}<a download href="{{ asset('storage/'.$product->document->path) }}" style="color: blue"> Скачать</a></p>
                     @endif
                 </div>
                 <div class="column-right flex flex-col justify-center">
                     @if ($product->image)
-                        <img class="" src="{{ asset('storage/'.$product->image) }}" alt="">
+                        <div class="img-wrapper w-[300px] h-[300px] contain">
+                            <img class="w-full" src="{{ asset('storage/'.$product->image ?? "-") }}" alt="">
+                        </div>
                     @endif
                 </div>
             </div>
